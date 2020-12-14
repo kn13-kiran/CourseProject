5d14ca3cc2b4ef562ef9fc0edd5f12b0a63a5da4
Kiran Nimashakavi <kn13@illinois.edu>
2020-12-13 17:08:47 -0800

    inlining classifier

--- menu_classifier.py
+++ menu_classifier.py
@@ -9,13 +9,8 @@ class MenuClassifier(object):
 		self.fwdIndex = metapy.index.make_forward_index(configFile)
 
 		# Define multi class data set
-		#print(configFile,self.fwdIndex,self.invertedIndex)
-		#print("labels",self.fwdIndex.num_labels())
 		self.multiClassDataset = metapy.classify.MulticlassDataset(self.fwdIndex)
-		self.classifier = self.getClassifier(self.multiClassDataset, self.fwdIndex, self.invertedIndex)
-
-	def getClassifier(self, training, fwdIndex, invertedIndex):
-		return metapy.classify.NaiveBayes(training=training, alpha=0.01, beta=0.01)
+		self.classifier = metapy.classify.NaiveBayes(self.multiClassDataset, alpha=0.01, beta=0.01)
 
 	def score(self, link_text, page_title, body_text):
 		doc = metapy.index.Document()
